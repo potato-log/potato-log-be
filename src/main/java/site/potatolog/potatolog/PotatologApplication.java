edcc94bcfdcff73199538a6cd9c5d343a025706c
imjeongjin <kjj_010101@naver.com>
2024-05-07 23:32:22 +0900

    Chore: 초기세팅

---
+++ src/main/java/site/potatolog/potatolog/PotatologApplication.java
@@ -0,0 +1,13 @@
+package site.potatolog.potatolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotatologApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotatologApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/potatolog/potatolog/PotatologApplicationTests.java
@@ -0,0 +1,13 @@
+package site.potatolog.potatolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotatologApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
