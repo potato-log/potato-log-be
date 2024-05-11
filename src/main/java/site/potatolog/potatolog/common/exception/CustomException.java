1cf16f8e6029986135f7e0b1a36659c8722f8005
JeYeongR <wpdud2003@gmail.com>
2024-05-11 20:05:04 +0900

    Fix: 커스텀 예외 생성자 접근제어자 변경

--- src/main/java/site/potatolog/potatolog/common/exception/CustomException.java
+++ src/main/java/site/potatolog/potatolog/common/exception/CustomException.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 public class CustomException extends RuntimeException {
   private final ErrorCode errorCode;
 
-  public CustomException(ErrorCode errorCode) {
+  protected CustomException(ErrorCode errorCode) {
     super(errorCode.getMessage());
     this.errorCode = errorCode;
   }
