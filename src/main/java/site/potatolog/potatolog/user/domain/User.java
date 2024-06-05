837f5744c8979792f69b4f644c3c88f31d288155
chaseunghyeok <xvds1@naver.com>
2024-06-05 15:35:02 +0900

    게ìFeat: 글 작성 api

--- src/main/java/site/potatolog/potatolog/user/domain/User.java
+++ src/main/java/site/potatolog/potatolog/user/domain/User.java
@@ -1,26 +1,28 @@
 package site.potatolog.potatolog.user.domain;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import site.potatolog.potatolog.common.domain.BaseEntityWithIsDeleted;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "users")
+@ToString
 public class User extends BaseEntityWithIsDeleted {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "github_id")
+    private Integer githubId;
+
     @Column(nullable = false, length = 20)
     private String nickname;
 
     @Column(nullable = false, length = 100)
-    private String email;
+    private String address;
 
     @Column(name = "social_account_uid", nullable = false)
     private String socialAccountUid;
@@ -39,5 +41,15 @@ public class User extends BaseEntityWithIsDeleted {
 
     @Column(name = "following_count", nullable = false)
     private int followingCount;
+
+    @Builder
+    public User(Integer githubId, String nickname, String address, String socialAccountUid, String profileImageUrl, String blogName) {
+        this.githubId = githubId;
+        this.nickname = nickname;
+        this.address = address;
+        this.socialAccountUid = socialAccountUid;
+        this.profileImageUrl = profileImageUrl;
+        this.blogName = blogName != null ? blogName : "DefaultBlogName";
+    }
 
 }
